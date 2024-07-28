cd7d85bf0be2674e4a6c0dc13ebeff99b9e32b1f
firlyaulia2 <afirlyaulia@gmail.com>
2024-07-29 00:20:46 +0700

    update

--- app.py
+++ app.py
@@ -33,13 +33,7 @@ cursor = db.cursor()
 
 jwt = JWTManager(app)
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'afirlyaulia@gmail.com'
-app.config['MAIL_PASSWORD'] = 'vuea elba bxze boxf'
-app.config['MAIL_DEFAULT_SENDER'] = 'afirlyaulia@gmail.com'
+
 
 mail = Mail(app)
 
